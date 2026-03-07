01c36819954e83bc3de9120f084db253d4a092ad
Muaz osama <moazosama204@gmail.com>
2026-03-07 05:49:13 +0200

    add splash screen with Lottie animation and set up Compose navigation

---
+++ app/src/main/java/com/example/afaq/presentation/navigation/AppNavigation.kt
@@ -0,0 +1,42 @@
+package com.example.afaq.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.afaq.presentation.home.screens.HomeScreen
+import com.example.afaq.presentation.splash.screens.SplashScreen
+
+@Composable
+fun AppNavigation(
+    navController : NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = Routes.SplashRoute
+    ){
+        composable<Routes.SplashRoute> {
+            SplashScreen (
+                modifier = modifier
+            ){
+                navController.navigate(Routes.HomeRoute) {
+                    popUpTo(Routes.SplashRoute) {
+                        inclusive = true
+                    }
+                }
+            }
+        }
+
+        composable<Routes.HomeRoute> {
+            HomeScreen(
+                modifier = modifier
+            )
+        }
+    }
+}
+
+// 1. create routes
+// 2. nav controller
+// 3. nav host
--- app/src/main/java/com/example/afaq/presentation/theme/theme/Type.kt
+++ app/src/main/java/com/example/afaq/presentation/theme/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.afaq.ui.theme
+package com.example.afaq.presentation.theme.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
